046abbdea98f4760c761391a38fab476ef528cc3
Denys Bokov <denisbokov1703@gmail.com>
2024-02-08 21:26:05 +0100

    base card styles markup

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,9 +5,36 @@ type Props = {
 }
 
 export const Card: React.FC<Props> = ({card}) => {
+  console.log(card);
+  const { author, title, created_at, url} = card;
   return (
     <div key={card.author} className={styles.card}>
-      {card.title}
+      <div className={styles.container}>
+        <div>
+          <span>Author: </span>
+          <span>
+            {author}
+          </span>
+        </div>
+
+        <div>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div>
+          <span>{title}</span>
+        </div>
+
+
+        <div>
+          <span>Created at: </span>
+          <span>
+            {created_at}
+          </span>
+        </div>
+      </div>
+      <a href={url} target="_blank">Follow</a>
     </div>
   )
 }
